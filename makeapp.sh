9555c3d985e7b0cb64894575a27de98f49bf7c43
stefan-hoeck <hock@zhaw.ch>
2026-04-30 16:25:52 +0200

    [ admin ] restructure project

--- makeapp.sh
+++ makeapp.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
-APP_CSS="css/app.css"
+APP_CSS="draw/app.css"
 curl https://cdn.jsdelivr.net/npm/modern-normalize/modern-normalize.min.css > "$APP_CSS"
 pack --log-level silence exec cyby-css/src/CyBy/UI/CSS/Rules.idr >> "$APP_CSS"
 pack build cyby-draw-app
+cp app/build/exec/cyby-draw-app.js draw/
