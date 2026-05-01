f0fe1ba7ed6255e8ba7dad3a5e8eef4fcb476114
stefan-hoeck <hock@zhaw.ch>
2026-05-01 04:29:59 +0200

    [ fix ] create draw dir if it does not exist in makeapp.sh

--- makeapp.sh
+++ makeapp.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
+mkdir -p draw
 APP_CSS="draw/app.css"
 curl https://cdn.jsdelivr.net/npm/modern-normalize/modern-normalize.min.css > "$APP_CSS"
 pack --log-level silence exec cyby-css/src/CyBy/UI/CSS/Rules.idr >> "$APP_CSS"
