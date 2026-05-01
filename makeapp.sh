98a2ddfda238d9dcd2fe7217d8464c5cb8508099
Stefan Höck <hock@zhaw.ch>
2026-05-01 02:53:43 +0000

    [ build ] add makecss.sh utility script (#99)

--- makeapp.sh
+++ makeapp.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env sh
-mkdir -p draw
-APP_CSS="draw/app.css"
-curl https://cdn.jsdelivr.net/npm/modern-normalize/modern-normalize.min.css > "$APP_CSS"
-pack --log-level silence exec cyby-css/src/CyBy/UI/CSS/Rules.idr >> "$APP_CSS"
+./makecss.sh
 pack build cyby-draw-app
 cp app/build/exec/cyby-draw-app.js draw/
